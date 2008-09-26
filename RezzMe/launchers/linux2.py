e4d7c3d37f82650c15201ec449ee6ffd66526654
drscofield <drscofield@7bc2bfa8-0109-4806-b14c-dc71ee9d10d6>
2008-09-26 14:37:34 +0000

    support for locating and using hippo
    git-svn-id: http://forge.opensimulator.org/svn/rezzme/trunk@30 7bc2bfa8-0109-4806-b14c-dc71ee9d10d6

--- RezzMe/launchers/linux2.py
+++ RezzMe/launchers/linux2.py
@@ -27,11 +27,25 @@
 # OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+from __future__ import with_statement
+
 import logging
 import os
 import urllib
+import lxml.etree as ET
 
-client = 'secondlife'
+import RezzMe.exceptions
+
+clients = ['hippo_opensim_viewer', 'secondlife']
+
+def FindClient(clients):
+    for bin in os.environ['PATH'].split(':'):
+        for c in clients:
+            c = '%s/%s' % (bin, c)
+            if os.path.exists(c):
+                return c
+    return None
+
 
 def Launch(avatar, password, gridInfo, location):
     clientArgs = [ ]
@@ -50,9 +64,81 @@ def Launch(avatar, password, gridInfo, location):
     if location:
         clientArgs += [location]
 
+    # locate client:
+    client = FindClient(clients)
+    if not client: 
+        logging.critical('RezzMe.launchers.linux2: did not find %s on path %s', ' or '.join(clients), sys.environ['PATH'])
+        raise RezzMe.exceptions.RezzMeException('cannot find suitable client! install hippo viewer or secondlife client and try again')
+
+    logging.debug('RezzMe.launchers.linux2: found client %s', client)
+
+    if 'hippo' in client:
+        hippoGridInfo = None
+        hippoUserSettings = os.path.expanduser('~/.hippo_opensim_viewer/user_settings')
+        hippoGridInfoXml = os.path.expanduser('~/.hippo_opensim_viewer/user_settings/grid_info.xml') 
+
+        # hippo 0.21 doesn't honor -loginuri :-( so, we need to write the gridinfo.xml file
+        if not os.path.exists(hippoUserSettings):
+            logging.info('RezzMe.launchers.linux2: hippo: creating hippo setting directory %s', hippoUserSettings)
+            os.makedirs(hippoUserSettings)
+
+        if os.path.exists(hippoGridInfoXml):
+            logging.debug('RezzMe.launchers.linux2: hippo: found %s', hippoGridInfoXml)
+            with open(hippoGridInfoXml, 'r') as xml:
+                hippoGridInfo = ET.parse(xml)
+        else:
+            logging.debug('RezzMe.launchers.linux2: hippo: hippo grid info not found at %s, creating it', hippoGridInfoXml)
+            hippoGridInfo = ET.fromstring('<llsd><array></array></llsd>')
+            
+        # check whether we are already in hippo's grid info
+        gridnick = None
+
+        grid = hippoGridInfo.xpath('/llsd/array/map/string[string() = "%s"]' % gridInfo['login'])
+        if grid and grid[0].text == gridInfo['login']:
+            # LLSD "XML" sucks big time: it depends on the ordering of elements to work...
+            gridnick = grid[0].getparent().xpath('./key[string() = "gridnick"]')[0].getnext().text
+            logging.debug('RezzMe.launchers.linux2: hippo:  found gridnick %s for loginuri %s', 
+                          gridnick, gridInfo['login'])
+        else:
+            logging.debug('RezzMe.launchers.linux2: hippo:  no existing gridnick for loginuri %s', gridInfo['login'])
+            gridXml = '<map>'
+            for (key, value) in {'gridname' : 'gridname', 
+                                 'gridnick' : 'gridnick', 
+                                 'economy'  : 'helperuri', 
+                                 'welcome'  : 'loginpage', 
+                                 'login'    : 'loginuri', 
+                                 'platform' : 'platform', 
+                                 'about'    : 'website'}.iteritems():
+                if key in gridInfo: 
+                    gridXml += '<key>%s</key><string>%s</string>' % (value, gridInfo[key])
+                else:
+                    gridXml += '<key>%s</key><string/>' % value
+            gridXml += '</map>'
+            grid = ET.fromstring(gridXml)
+            logging.debug('RezzMe.launchers.linux2: hippo: : adding XML sniplet %s to grid_info.xml', 
+                          ET.tostring(grid, pretty_print = True))
+
+            # write out grid_info.xml again
+            hippoGridInfo.xpath('/llsd/array')[0].append(grid)
+            logging.debug('RezzMe.launchers.linux2: hippo: grid_info.xml: %s', 
+                          ET.tostring(hippoGridInfo))
+
+            if os.path.exists(hippoGridInfoXml):
+                bak = '%s.bak' % hippoGridInfoXml
+                logging.debug('RezzMe.launchers.linux2: hippo: "%s" exists, baking up to "%s"', 
+                              hippoGridInfoXml, bak)
+                if os.path.exists(bak): os.unlink(bak)
+                os.rename(hippoGridInfoXml, bak)
+            hippoGridInfo.write(hippoGridInfoXml)
+            logging.info('RezzMe.launchers.linux2: hippo: updated grid_info.xml')
+
+            gridnick = gridInfo['gridnick']
+        
+        clientArgs += ['-grid', gridnick]
+
     # all systems go: start client
     clientArgs = [ client ] + clientArgs
-    logging.debug('RezzMe.launchers.linux2.Launch: client %s args %s', client, ' '.join(clientArgs))
+    logging.debug('RezzMe.launchers.linux2: client %s args %s', client, ' '.join(clientArgs))
     os.execvp(client, clientArgs)
 
 
